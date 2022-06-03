3d4d058f7c2af65e6bbbc3b969f92d1a50c4d335
Marianna32 <marianna.boshoff@gmail.com>
2022-06-03 11:16:59 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,26 @@
   */
 int main(void)
 {
-	int c, i;
+    int c, i;
 
-	c = i = '0';
+    c = i = '0';
 
-	for (c = '0'; c <= '9'; c++)
+    for (c = '0'; c <= '9'; c++)
+    {
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (i = '0'; i <= '9'; i++)
-		{
-			putchar(c);
-			putchar(i);
+	    putchar(c);
+	    putchar(i);
 
-			if ((c != '9') || (c == '9' && i != '9'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+	    if ((c != '9') || (c == '9' && i != '9'))
+	    {
+		putchar(',');
+		putchar(' ');
+	     }
+       }
+}
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
