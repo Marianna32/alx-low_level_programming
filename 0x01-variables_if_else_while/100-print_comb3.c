33f820601bb34ca197761a36e8c036e768ad63a1
Marianna32 <marianna.boshoff@gmail.com>
2022-06-03 11:08:19 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,27 +7,27 @@
   */
 int main(void)
 {
-	int c, i;
+    int c, i;
 
-	for (c = '0'; c <= '9'; c++)
+    for (c = '0'; c <= '9'; c++)
+    {
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (i = '0'; i <= '9'; i++)
-		{
-			if (c < i)
-			{
-				putchar(c);
-				putchar(i);
+	     if (c < i)
+	     {
+		putchar(c);
+		putchar(i);
 
-				if (c != '8' || (c == '8' && i != '9'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+	        if (c != '8' || (c == '8' && i != '9'))
+		{
+		   putchar(',');
+		   putchar(' ');
 		}
 	}
+    }
+}
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
