602b33ce20bf1b1be6f3a3f997636d66e8ceed48
Marianna32 <marianna.boshoff@gmail.com>
2022-06-03 10:16:26 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,17 @@
   */
 int main(void)
 {
-	char c;
+    char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+    for (c = 'a'; c <= 'z'; c++)
+    {
+	if (c != 'e' && c != 'q')
 	{
-		if (c != 'e' && c != 'q')
-		{
-			putchar(c);
-		}
+	   putchar(c);
 	}
+     }
 
-	putchar('\n');
+     putchar('\n');
 
-	return (0);
+     return (0);
 }
