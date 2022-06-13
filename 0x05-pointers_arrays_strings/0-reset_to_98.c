c904b809f2a9c343d443ed9c3e0848dc3cb43630
Marianna32 <marianna.boshoff@gmail.com>
2022-06-13 10:39:30 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - check the code for ALX students.
- * @n: is a pointer int type
- * Return: Always 0.
- */
+* reset_to_98 - check the code for ALX students.
+* @n: is a pointer int type
+* Return: Always 0.
+*/
 
 void reset_to_98(int *n)
 {
